f0d444e0fb1892f7a9fc0379bfcf1b3c198b082e
Sam Selikoff <sam.selikoff@gmail.com>
2020-07-17 08:30:06 -0400

    Beginning of video

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,82 @@
-import Head from 'next/head'
+import useSWR from "swr";
+import { format, parseISO } from "date-fns";
 
-export default function Home() {
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+function Home() {
+  let { error, data } = useSWR("/api/tweets", fetcher);
+
+  if (error) return "An error has occurred.";
+  if (!data)
+    return (
+      <p className="px-4 py-2 mt-32 text-lg font-medium text-center text-gray-400">
+        Loading...
+      </p>
+    );
+
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      <div className="divide-y divide-gray-200">
+        {[...data?.tweets].reverse().map((tweet) => (
+          <div className="px-4 py-2" key={tweet.id}>
+            <Tweet tweet={tweet} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+Home.headerTitle = "Latest Tweets";
+Home.headerBorder = true;
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+export default Home;
+
+function Tweet({ tweet }) {
+  return (
+    <div className="flex">
+      <img className="w-12 h-12 mr-3 rounded-full" src={tweet.avatarUrl} />
+
+      <div className="flex flex-col min-w-0">
+        <p className="flex items-center max-w-full font-bold">
+          <span className="overflow-hidden whitespace-no-wrap">
+            <span>{tweet.name}</span>
+            <span className="pl-3 font-normal text-gray-500">
+              @{tweet.username}
+            </span>
+          </span>
+          <span className="flex-shrink-0 font-normal text-gray-500">
+            <span className="px-1 text-xs text-gray-500">·</span>
+            {format(parseISO(tweet.date), "MMM d")}
+          </span>
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <p className="text-sm">{tweet.text}</p>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="flex justify-between mt-3 text-gray-500">
+          <svg viewBox="0 0 24 24" className="w-5 h-5 p-px fill-current">
+            <g>
+              <path d="M14.046 2.242l-4.148-.01h-.002c-4.374 0-7.8 3.427-7.8 7.802 0 4.098 3.186 7.206 7.465 7.37v3.828c0 .108.044.286.12.403.142.225.384.347.632.347.138 0 .277-.038.402-.118.264-.168 6.473-4.14 8.088-5.506 1.902-1.61 3.04-3.97 3.043-6.312v-.017c-.006-4.367-3.43-7.787-7.8-7.788zm3.787 12.972c-1.134.96-4.862 3.405-6.772 4.643V16.67c0-.414-.335-.75-.75-.75h-.396c-3.66 0-6.318-2.476-6.318-5.886 0-3.534 2.768-6.302 6.3-6.302l4.147.01h.002c3.532 0 6.3 2.766 6.302 6.296-.003 1.91-.942 3.844-2.514 5.176z"></path>
+            </g>
+          </svg>
+          <svg viewBox="0 0 24 24" className="w-5 h-5 p-px fill-current">
+            <g>
+              <path d="M23.77 15.67c-.292-.293-.767-.293-1.06 0l-2.22 2.22V7.65c0-2.068-1.683-3.75-3.75-3.75h-5.85c-.414 0-.75.336-.75.75s.336.75.75.75h5.85c1.24 0 2.25 1.01 2.25 2.25v10.24l-2.22-2.22c-.293-.293-.768-.293-1.06 0s-.294.768 0 1.06l3.5 3.5c.145.147.337.22.53.22s.383-.072.53-.22l3.5-3.5c.294-.292.294-.767 0-1.06zm-10.66 3.28H7.26c-1.24 0-2.25-1.01-2.25-2.25V6.46l2.22 2.22c.148.147.34.22.532.22s.384-.073.53-.22c.293-.293.293-.768 0-1.06l-3.5-3.5c-.293-.294-.768-.294-1.06 0l-3.5 3.5c-.294.292-.294.767 0 1.06s.767.293 1.06 0l2.22-2.22V16.7c0 2.068 1.683 3.75 3.75 3.75h5.85c.414 0 .75-.336.75-.75s-.337-.75-.75-.75z"></path>
+            </g>
+          </svg>
+          <svg viewBox="0 0 24 24" className="w-5 h-5 p-px fill-current">
+            <g>
+              <path d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.034 11.596 8.55 11.658 1.518-.062 8.55-5.917 8.55-11.658 0-2.267-1.823-4.255-3.903-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.014-.03-1.425-2.965-3.954-2.965z"></path>
+            </g>
+          </svg>
+          <svg viewBox="0 0 24 24" className="w-5 h-5 p-px fill-current">
+            <g>
+              <path d="M17.53 7.47l-5-5c-.293-.293-.768-.293-1.06 0l-5 5c-.294.293-.294.768 0 1.06s.767.294 1.06 0l3.72-3.72V15c0 .414.336.75.75.75s.75-.336.75-.75V4.81l3.72 3.72c.146.147.338.22.53.22s.384-.072.53-.22c.293-.293.293-.767 0-1.06z"></path>
+              <path d="M19.708 21.944H4.292C3.028 21.944 2 20.916 2 19.652V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 .437.355.792.792.792h15.416c.437 0 .792-.355.792-.792V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 1.264-1.028 2.292-2.292 2.292z"></path>
+            </g>
+          </svg>
         </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      </div>
     </div>
-  )
+  );
 }
---
+++ server.js
@@ -0,0 +1,62 @@
+import { createServer, Model, Factory, trait } from "miragejs";
+import { add, parseISO } from "date-fns";
+import faker, { image, name, internet, lorem } from "faker";
+
+faker.seed(123);
+
+let startingDate = parseISO("2020-01-14");
+let server = createServer({
+  timing: 1000,
+  models: {
+    tweet: Model,
+  },
+
+  factories: {
+    tweet: Factory.extend({
+      name() {
+        return name.findName();
+      },
+
+      username() {
+        return internet.userName();
+      },
+
+      text() {
+        return lorem.sentence();
+      },
+
+      avatarUrl() {
+        return image.avatar();
+      },
+
+      date(i) {
+        return add(startingDate, { days: i }).toISOString();
+      },
+
+      fromSam: trait({
+        name: "Sam Selikoff",
+        username: "samselikoff",
+        avatarUrl: "http://twivatar.glitch.me/samselikoff",
+      }),
+    }),
+  },
+
+  routes() {
+    this.namespace = "api";
+    this.get("tweets");
+
+    this.passthrough();
+  },
+
+  seeds(server) {
+    server.create("tweet", "fromSam", { text: "just setting up my twttr" });
+    server.create("tweet", "fromSam", { text: "Hi" });
+    server.create("tweet", "fromSam", {
+      text: "I still don't understand useEffect",
+    });
+  },
+});
+
+// setInterval(() => {
+//   server.create("tweet");
+// }, 3000);
